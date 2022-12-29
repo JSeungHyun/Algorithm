80086ba21388012dddf66bb585bd51543a0be173
JSeungHyun <83762088+JSeungHyun@users.noreply.github.com>
2022-12-29 12:14:16 +0900

    [Silver II] Title: 점프 점프, Time: 56 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.py
+++ 백준/Silver/11060. 점프 점프/점프 점프.py
@@ -11,7 +11,6 @@ for i in range(N):
     for j in range(1,graph[i]+1):
         if i+j < N:
             dp[i+j] = min(dp[i]+1,dp[i+j])
-        else:
-            break
+        
 
 print(-1 if dp[N-1] == N+1 else dp[N-1])
