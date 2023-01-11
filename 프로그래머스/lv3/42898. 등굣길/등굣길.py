823b64acca07ad9018b956ea27a0bc526147033f
JSeungHyun <83762088+JSeungHyun@users.noreply.github.com>
2023-01-11 23:04:07 +0900

    [level 3] Title: 등굣길, Time: 1.61 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -0,0 +1,21 @@
+def solution(m, n, puddles):
+    answer = 0
+    graph = [[1] * m for _ in range(n)]
+    for a,b in puddles:
+        graph[b-1][a-1] = 0
+    tmp = 1
+    for i in range(m):
+        if graph[0][i] == 0:
+            tmp = 0
+        graph[0][i] = tmp
+    tmp = 1
+    for i in range(n):
+        if graph[i][0] == 0:
+            tmp = 0
+        graph[i][0] = tmp
+    
+    for y in range(1,n):
+        for x in range(1,m):
+            if graph[y][x] != 0:
+                graph[y][x] = (graph[y-1][x] + graph[y][x-1]) % 1000000007
+    return graph[n-1][m-1] % 1000000007
