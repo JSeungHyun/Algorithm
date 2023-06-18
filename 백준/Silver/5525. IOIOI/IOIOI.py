5690d94f9438c2f7517c1d6d4ac43b5f397d28f6
JSeungHyun <83762088+JSeungHyun@users.noreply.github.com>
2023-06-18 15:39:05 +0900

    [Silver I] Title: IOIOI, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+res = 0
+n = int(input())
+m = int(input())
+s = input().rstrip()
+k = 'I' + 'OI' * n
+for i in range(m - (2 * n)):
+    if s[i : (i + 2 * n) + 1] == k:
+        res += 1
+
+print(res)
