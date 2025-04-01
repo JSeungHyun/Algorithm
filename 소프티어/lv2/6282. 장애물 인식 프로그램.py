2f4dec2f25db7ae9984b73679f4cef2c416ca74d
Jeong <net1645@naver.com>
2025-04-01 23:41:45 +0900

    :zap:

--- 소프티어/lv2/6282. 장애물 인식 프로그램.py
+++ 소프티어/lv2/6282. 장애물 인식 프로그램.py
@@ -3,12 +3,46 @@ input = sys.stdin.readline
 
 
 N = int(input())
-visited = [[False] * N for _ in range(N)]
-blocks = [[int(char) for char in input().rstrip()] for _ in range(N)]
+# visited = [[False] * N for _ in range(N)]
+# blocks = [[int(char) for char in input().rstrip()] for _ in range(N)]
+# dy = [-1, 1, 0, 0]
+# dx = [0, 0, -1, 1]
+# stack = []
+# cnt = 0
+# result = []
 
-print(blocks)
-print(visited)
-# 1을 만났을때 해당 좌표를 배열에 넣는다.
-# while 배열: (배열의 size 0가 일때까지 반복)
-# 배열에서 좌표를 pop , 해당 좌표 방문처리
-# 해당 좌표의 상 하 좌 우 좌표를 배열에 추가
+# for y in range(N):
+#     for x in range(N):
+#         if blocks[y][x] == 0: # 장애물이 없으면
+#             continue
+#         if visited[y][x] == True: # 이미 방문한 곳이면
+#             continue
+        
+#         # 탐색 시작
+#         block_count = 0
+#         cnt += 1
+#         stack.append((y, x))
+#         visited[y][x] = True # 방문처리
+#         while stack:
+#             block_count += 1
+#             cy, cx = stack.pop(0)
+            
+
+#             for i in range(4):
+#                 ny = cy + dy[i]
+#                 nx = cx + dx[i]
+
+#                 if ny < 0 or nx < 0 or ny >= N or nx >= N:
+#                     continue
+#                 if visited[ny][nx] == False and blocks[ny][nx] == 1:
+#                     visited[ny][nx] = True # 방문처리
+#                     stack.append((ny, nx))
+
+#         result.append(block_count)
+
+# print(cnt)
+# for x in sorted(result):
+#     print(x)
+
+map = [list(map(int, input().split())) for _ in range(N)]
+print(map)
