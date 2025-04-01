374d5230bfe9b5bb07bb55d00bb97ee4e50c34e4
Jeong <net1645@naver.com>
2025-04-01 23:47:38 +0900

    :zap:

--- 소프티어/lv2/6282. 장애물 인식 프로그램.py
+++ 소프티어/lv2/6282. 장애물 인식 프로그램.py
@@ -3,46 +3,42 @@ input = sys.stdin.readline
 
 
 N = int(input())
-# visited = [[False] * N for _ in range(N)]
-# blocks = [[int(char) for char in input().rstrip()] for _ in range(N)]
-# dy = [-1, 1, 0, 0]
-# dx = [0, 0, -1, 1]
-# stack = []
-# cnt = 0
-# result = []
+visited = [[False] * N for _ in range(N)]
+blocks = [[int(char) for char in input().rstrip()] for _ in range(N)]
 
-# for y in range(N):
-#     for x in range(N):
-#         if blocks[y][x] == 0: # 장애물이 없으면
-#             continue
-#         if visited[y][x] == True: # 이미 방문한 곳이면
-#             continue
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
+stack = []
+result = []
+
+for y in range(N):
+    for x in range(N):
+        if blocks[y][x] == 0: # 장애물이 없으면
+            continue
+        if visited[y][x] == True: # 이미 방문한 곳이면
+            continue
         
-#         # 탐색 시작
-#         block_count = 0
-#         cnt += 1
-#         stack.append((y, x))
-#         visited[y][x] = True # 방문처리
-#         while stack:
-#             block_count += 1
-#             cy, cx = stack.pop(0)
+        # 탐색 시작
+        block_count = 0
+        stack.append((y, x))
+        visited[y][x] = True # 방문처리
+        while stack:
+            block_count += 1
+            cy, cx = stack.pop(0)
             
+            for i in range(4):
+                ny = cy + dy[i]
+                nx = cx + dx[i]
 
-#             for i in range(4):
-#                 ny = cy + dy[i]
-#                 nx = cx + dx[i]
+                if ny < 0 or nx < 0 or ny >= N or nx >= N:
+                    continue
+                
+                if visited[ny][nx] == False and blocks[ny][nx] == 1:
+                    visited[ny][nx] = True # 방문처리
+                    stack.append((ny, nx))
 
-#                 if ny < 0 or nx < 0 or ny >= N or nx >= N:
-#                     continue
-#                 if visited[ny][nx] == False and blocks[ny][nx] == 1:
-#                     visited[ny][nx] = True # 방문처리
-#                     stack.append((ny, nx))
+        result.append(block_count)
 
-#         result.append(block_count)
-
-# print(cnt)
-# for x in sorted(result):
-#     print(x)
-
-map = [list(map(int, input().split())) for _ in range(N)]
-print(map)
+print(len(result))
+for x in sorted(result):
+    print(x)
