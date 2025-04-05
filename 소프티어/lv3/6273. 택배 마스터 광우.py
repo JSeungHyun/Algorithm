a457531d3ee38cf23e4bb0dec520f7bb8fe44268
Jeong <net1645@naver.com>
2025-04-05 10:47:18 +0900

    :zap:

--- 소프티어/lv3/6273. 택배 마스터 광우.py
+++ 소프티어/lv3/6273. 택배 마스터 광우.py
@@ -3,5 +3,28 @@ input = sys.stdin.readline
 from itertools import permutations
 
 N, M, K = map(int, input().split())
-boxes = list(map(int, input().split()))
-print(*permutations(boxes, N))
+mix = list(permutations(list(map(int, input().split())), N))
+result = sys.maxsize
+
+for i in range(len(mix)):
+    idx = 0 # 현재 담을 박스 위치
+    cnt = 0 # 일의 진행 수
+    boxes = mix[i]
+    weight = 0 # 이 순서로 진행했을때의 무게
+    temp = 0
+    while cnt < K:
+        temp += boxes[idx % N]
+        idx += 1
+        if temp + boxes[(idx) % N] > M: # 다음 택배를 담을 수 없으면
+            weight += temp
+            temp = 0
+            cnt += 1
+
+            if weight > result:
+                weight = sys.maxsize
+                cnt = sys.maxsize
+
+    result = min(result, weight)
+
+
+print(result)
