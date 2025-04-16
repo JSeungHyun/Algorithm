05a3f91715773101ca31457b8f88142258f66760
JeongSeungHyun <net1645@gmail.com>
2025-04-16 13:42:35 +0900

    :zap:

--- 소프티어/lv3/6289. 우물 안 개구리.py
+++ 소프티어/lv3/6289. 우물 안 개구리.py
@@ -1,43 +1,18 @@
 import sys
-from collections import defaultdict, deque
-
 input = sys.stdin.readline
 
-# 입력 받기
 N, M = map(int, input().split())
 W = list(map(int, input().split()))
+best = [True for _ in range(N)]
 
-
-graph = defaultdict(list)
 for _ in range(M):
-    a, b = map(int, input().split())
-    a -= 1
-    b -= 1
-    graph[a].append(b)
+    a, b = map(lambda x: int(x) - 1, input().split())
+    if W[a] > W[b]:
+        best[b] = False
+    elif W[a] < W[b]:
+        best[a] = False
+    else:
+        best[b] = False
+        best[a] = False
 
-visited = [False] * N
-result = 0
-
-def bfs(start):
-    queue = deque([start])
-    visited[start] = True
-    
-    while queue:
-        node = queue.popleft()
-        for n in graph[node]:
-            if not visited[n]:
-                visited[n] = True
-                queue.append(n)
-
-# 그래프에 있는 노드들에 대해 연결 요소 확인
-for node in range(N):
-    if node in graph and not visited[node]:
-        bfs(node)
-        result += 1
-
-# 방문되지 않은 노드들 카운트
-for node in range(N):
-    if not visited[node]:
-        result += 1
-
-print(result)
+print(best.count(True))
