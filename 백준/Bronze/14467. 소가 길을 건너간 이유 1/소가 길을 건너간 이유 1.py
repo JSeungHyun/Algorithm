85027877921fb252286f9a4639fcd2afb1b232f9
JSeungHyun <83762088+JSeungHyun@users.noreply.github.com>
2025-05-04 12:18:26 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 52 ms, Memory: 34908 KB -BaekjoonHub

---
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+from collections import Counter
+
+N = int(input())
+cows = {}
+result = 0
+
+for _ in range(N):
+    c, l = map(int, input().split())
+    if c not in cows:
+        cows[c] = l
+    elif cows[c] != l:
+        cows[c] = l
+        result += 1
+
+print(result)
