ad0372b8416d89d9a3a2467c8b3b36262dcf8c2a
JSeungHyun <83762088+JSeungHyun@users.noreply.github.com>
2026-09-17 16:52:52 +0900

    [level 1] Title: 폰켓몬, Time: 1.92 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int n = nums.length / 2;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num);
+        
+        if (set.size() >= n) return n;
+        return set.size();
+    }
+}
